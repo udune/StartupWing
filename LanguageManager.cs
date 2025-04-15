f6d865210c70aa10bb2e1f5ba43165e6267101e7
udune <78453968+udune@users.noreply.github.com>
2025-04-16 01:31:12 +0900

    Update LanguageManager.cs

--- LanguageManager.cs
+++ LanguageManager.cs
@@ -10,206 +10,160 @@ public class LanguageManager : MonoBehaviour
     private class Language
     {
         public string Code;
-        public List<string> Languages = new List<string>();
+        public List<string> Translations = new List<string>();
     }
 
     private static LanguageManager instance;
-
-    private static LanguageManager Instance
-    {
-        get => instance == null ? instance = new GameObject(typeof(LanguageManager).Name).AddComponent<LanguageManager>() : instance;
-    }
+    private static bool initialized = false;
 
     private static List<Language> languages;
     private static List<string> countryCodes;
-    private static Dictionary<string, List<Language>> npcMessage;
+    private static Dictionary<string, List<Language>> npcMessages;
 
-    // 0 : KR , 1 : EN,  2: JP
-    private static int index = -1;
-    private static bool initialized = false;
+    // 0: KR, 1: EN, 2: JP
+    private static int currentLanguageIndex = -1;
 
-    public static int Index
+    private const string LANGUAGE_KEY = "LANGUAGE_KEY_V1";
+
+    public static event Action OnLanguageCreated;
+    public static event Action OnLanguageChanged;
+
+    private static LanguageManager Instance
     {
-        get => index;
+        get
+        {
+            if (instance == null)
+            {
+                instance = new GameObject(typeof(LanguageManager).Name).AddComponent<LanguageManager>();
+            }
+            return instance;
+        }
+    }
+
+    public static int CurrentLanguageIndex
+    {
+        get => currentLanguageIndex;
         set => SetLanguage(value);
     }
 
-    const string LANGUAGE_KEY = "LANGUAGE_KEY_V1";
-
-    public static event Action OnCreatedLanguage;
-    public static event Action OnChangedLanguage;
+    public static bool IsInitialized => initialized;
 
     public static void Initialize(string url)
     {
         if (initialized)
             return;
 
-        Instance.StartCoroutine(GetLanguageCorutine(url));
+        Instance.StartCoroutine(FetchLanguageData(url));
+    }
 
-        IEnumerator GetLanguageCorutine(string _url)
+    private static IEnumerator FetchLanguageData(string url)
+    {
+        string baseUrl = url.Contains("/edit") ? url.Split(new[] { "/edit" }, StringSplitOptions.None)[0] : url;
+        string gid = ExtractGidFromUrl(url);
+
+        string languageUrl = $"{baseUrl}/export?gid={gid}&format=tsv";
+
+        using (UnityWebRequest www = UnityWebRequest.Get(languageUrl))
         {
-            // '/edit'의 존재 여부를 확인하고, 존재한다면 해당 부분을 기준으로 URL을 분리합니다.
-            string url = "";
-            string gid = "";
-
-            if (_url.Contains("/edit"))
-            {
-                string[] parts = _url.Split(new string[] { "/edit" }, StringSplitOptions.None);
-                url = parts[0];
-
-                if (parts.Length > 1 && parts[1].Contains("#gid="))
-                {
-                    string[] subParts = parts[1].Split(new string[] { "#gid=" }, StringSplitOptions.None);
-                    if (subParts.Length > 1)
-                    {
-                        gid = subParts[1];
-                    }
-                }
-            }
-            else
-            {
-                // '/edit'가 없는 경우, gid 파트만 분리합니다.
-                if (_url.Contains("#gid="))
-                {
-                    string[] parts = _url.Split(new string[] { "#gid=" }, StringSplitOptions.None);
-                    url = parts[0];
-                    if (parts.Length > 1)
-                    {
-                        gid = parts[1];
-                    }
-                }
-            }
-
-            string URL = $"{url}/export?gid={gid}&format=tsv";
-
-            using (UnityWebRequest www = UnityWebRequest.Get(URL))
-            {
-                yield return www.SendWebRequest();
-                CreateLanguage(www.downloadHandler.text);
-#if !UNITY_EDITOR
-             //   InitializeLanguageTable(www.downloadHandler.text);
-#endif
-                initialized = true;
-            }
+            yield return www.SendWebRequest();
+            ParseLanguageData(www.downloadHandler.text);
+            initialized = true;
         }
     }
 
-    public static bool isInitialized()
+    private static string ExtractGidFromUrl(string url)
     {
-        return initialized;
-    }
-
-    public static string Get(string _index)
-    {
-        int code = -1;
-        try
+        if (url.Contains("#gid="))
         {
-            code = languages.FindIndex(item => item.Code == _index);
+            return url.Split(new[] { "#gid=" }, StringSplitOptions.None)[1];
         }
-        catch (Exception e)
-        {
-            Debug.LogError(e);
-        }
-
-        if (code < 0)
-            return "Wrong Code";
-
-        return languages[code].Languages[index].Replace("<N>", "\n");
+        return string.Empty;
     }
 
-    public static List<string> GetNPCMessageList(string sceneName)
-    {
-        List<string> messageList;
-        List<Language> language;
-
-        if (!npcMessage.TryGetValue(sceneName, out language))
-        {
-            language = languages.FindAll(o => o.Code.Contains(sceneName));
-        }
-
-        messageList = language.Select(o => o.Languages[index]).ToList();
-
-        for (int i = 0; i < messageList.Count; i++)
-        {
-            if (index == 2) // 일본어 데이터 load시 불필요 공백 및 \r 추가 제거
-            {
-                messageList[i] = messageList[i].Replace("\r", "");
-                messageList[i] = messageList[i].Replace(" ", "");
-            }
-
-            messageList[i] = messageList[i].Replace("<N>", "\n");
-        }
-
-        return messageList;
-    }
-
-    private static void SetLanguage(int value)
-    {
-        index = value;
-        PlayerPrefs.SetInt(LANGUAGE_KEY, index);
-#if !UNITY_EDITOR
-      //  SaveLangToLocalStorage(index);
-#endif
-        OnChangedLanguage?.Invoke();
-    }
-
-    public static void SetLanguage(string countryCode)
-    {
-        index = countryCodes.IndexOf(countryCode) - 1;
-
-        PlayerPrefs.SetInt(LANGUAGE_KEY, index);
-#if !UNITY_EDITOR
-      //  SaveLangToLocalStorage(index);
-#endif
-        OnChangedLanguage?.Invoke();
-    }
-
-
-    private static void CreateLanguage(string tsv)
+    private static void ParseLanguageData(string tsvData)
     {
         languages = new List<Language>();
         countryCodes = new List<string>();
-        npcMessage = new Dictionary<string, List<Language>>();
+        npcMessages = new Dictionary<string, List<Language>>();
 
-        // 행 갯수
-        string[] row = tsv.Split('\n');
-        int rowSize = row.Length;
+        var rows = tsvData.Split('\n');
+        var columnCount = rows[0].Split('\t').Length;
 
-        // 열 갯수 
-        int columnSize = row[0].Split('\t').Length;
-
-        for (int i = 0; i < rowSize; i++)
+        for (int i = 0; i < rows.Length; i++)
         {
-            Language lang = new Language();
+            var columns = rows[i].Split('\t');
+            var language = new Language { Code = columns[0] };
 
-            string[] column = row[i].Split('\t');
-            lang.Code = column[0];
-
-            // 국가 코드 등록
-            if (i < 1)
+            if (i == 0) // 첫 번째 행은 국가 코드
             {
-                for (int k = 0; k < column.Length; k++)
-                    countryCodes.Add(column[k].Trim());
+                countryCodes.AddRange(columns.Skip(1).Select(code => code.Trim()));
+            }
+            else
+            {
+                language.Translations.AddRange(columns.Skip(1).Take(columnCount - 1));
             }
 
-            // 언어 등록
-            for (int j = 1; j < columnSize; j++)
-                lang.Languages.Add(column[j]);
-
-            languages.Add(lang);
+            languages.Add(language);
         }
 
-        // 언어인덱스 설정
         SetLanguageIndex();
-        OnCreatedLanguage?.Invoke();
+        OnLanguageCreated?.Invoke();
     }
 
     private static void SetLanguageIndex()
     {
-        index = PlayerPrefs.GetInt(LANGUAGE_KEY, -1);
-
-        // 기존에 설정한 언어가 없을 경우, DefaultLanguage(KR)로 세팅
-        if (index < 0)
+        currentLanguageIndex = PlayerPrefs.GetInt(LANGUAGE_KEY, -1);
+        if (currentLanguageIndex < 0)
+        {
             SetLanguage(AppConfig.AppSettings.defaultLanguage);
+        }
+    }
+
+    private static void SetLanguage(int languageIndex)
+    {
+        currentLanguageIndex = languageIndex;
+        PlayerPrefs.SetInt(LANGUAGE_KEY, currentLanguageIndex);
+        OnLanguageChanged?.Invoke();
+    }
+
+    public static void SetLanguage(string countryCode)
+    {
+        currentLanguageIndex = countryCodes.IndexOf(countryCode) - 1;
+        PlayerPrefs.SetInt(LANGUAGE_KEY, currentLanguageIndex);
+        OnLanguageChanged?.Invoke();
+    }
+
+    public static string GetTranslation(string languageCode)
+    {
+        var language = languages.FirstOrDefault(lang => lang.Code == languageCode);
+        if (language == null)
+        {
+            Debug.LogError("Invalid Language Code");
+            return "Invalid Language Code";
+        }
+        return language.Translations[currentLanguageIndex].Replace("<N>", "\n");
+    }
+
+    public static List<string> GetNpcMessagesForScene(string sceneName)
+    {
+        if (!npcMessages.TryGetValue(sceneName, out var sceneMessages))
+        {
+            sceneMessages = languages.Where(lang => lang.Code.Contains(sceneName)).ToList();
+        }
+
+        var messages = sceneMessages.Select(lang => lang.Translations[currentLanguageIndex]).ToList();
+        return CleanMessages(messages);
+    }
+
+    private static List<string> CleanMessages(List<string> messages)
+    {
+        return messages.Select(message =>
+        {
+            if (currentLanguageIndex == 2) // 일본어 처리
+            {
+                message = message.Replace("\r", "").Replace(" ", "");
+            }
+            return message.Replace("<N>", "\n");
+        }).ToList();
     }
 }
