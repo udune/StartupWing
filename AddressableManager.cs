ef560d2b0fc5630cab1e4a90fb253cf89de33a99
udune <78453968+udune@users.noreply.github.com>
2025-04-16 01:29:59 +0900

    Update AddressableManager.cs

--- AddressableManager.cs
+++ AddressableManager.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
@@ -9,75 +8,80 @@ using UnityEngine.ResourceManagement.ResourceLocations;
 public class Labels
 {
     public readonly string Scene = "Scene";
-    
 }
 
 public class AddressableManager : MonoBehaviour
 {
-  
-    Labels labels;
-    AssetLabelReference lable;
-    // Start is called before the first frame update
+    private Labels labels;
+    private Dictionary<string, GameObject> preloadedObjects = new Dictionary<string, GameObject>();
+
     void Start()
     {
-       
+        // 초기화 로직이 필요하면 여기에 작성
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // 업데이트 로직이 필요하면 여기에 작성
     }
 
-    public void Load(string label, string name)
+    public void LoadAssetByLabel(string label, string assetName)
     {
-        
-        StartCoroutine(PreloadHazards(labels.Scene, "name"));
+        StartCoroutine(PreloadAssetsWithLabel(label, assetName));
     }
 
-    Dictionary<string, GameObject> _preloadedObjects = new Dictionary<string, GameObject>();
-    private IEnumerator PreloadHazards(string label, string name)
+    private IEnumerator PreloadAssetsWithLabel(string label, string assetName)
+    {
+        // 라벨과 관련된 리소스 위치들을 로드
+        var loadResourceLocationsHandle = Addressables.LoadResourceLocationsAsync(label);
+
+        // 리소스 위치 로드가 완료될 때까지 대기
+        yield return loadResourceLocationsHandle;
+
+        if (loadResourceLocationsHandle.Status == AsyncOperationStatus.Failed)
         {
-      
-            //find all the locations with label "SpaceHazards"
-            var loadResourceLocationsHandle
-                = Addressables.LoadResourceLocationsAsync("SpaceHazards");
-
-        AsyncOperationHandle h =  Addressables.LoadSceneAsync("");
-
-       
-
-            if (!loadResourceLocationsHandle.IsDone)
-                yield return loadResourceLocationsHandle;
-
-            //start each location loading
-            List<AsyncOperationHandle> opList = new List<AsyncOperationHandle>();
-
-            foreach (IResourceLocation location in loadResourceLocationsHandle.Result)
-            {
-                AsyncOperationHandle<GameObject> loadAssetHandle   = Addressables.LoadAssetAsync<GameObject>(location);
-                loadAssetHandle.Completed += obj => { _preloadedObjects.Add(location.PrimaryKey, obj.Result); };
-                opList.Add(loadAssetHandle);
-            }
-
-            //create a GroupOperation to wait on all the above loads at once. 
-            var groupOp = Addressables.ResourceManager.CreateGenericGroupOperation(opList);
-
-            if (!groupOp.IsDone)
-                yield return groupOp;
-
-            Addressables.Release(loadResourceLocationsHandle);
-
-            //take a gander at our results.
-            foreach (var item in _preloadedObjects)
-            {
-                Debug.Log(item.Key + " - " + item.Value.name);
-            }
+            Debug.LogError("리소스 위치 로드 실패.");
+            yield break;
         }
-    
 
-    void Instate()
+        // 각 자산을 비동기적으로 로딩 시작
+        List<AsyncOperationHandle> loadOperations = new List<AsyncOperationHandle>();
+
+        foreach (IResourceLocation location in loadResourceLocationsHandle.Result)
+        {
+            var loadAssetHandle = Addressables.LoadAssetAsync<GameObject>(location);
+            loadAssetHandle.Completed += (handle) =>
+            {
+                if (handle.Status == AsyncOperationStatus.Succeeded)
+                {
+                    preloadedObjects.Add(location.PrimaryKey, handle.Result);
+                }
+                else
+                {
+                    Debug.LogError($"위치 {location.PrimaryKey}에서 자산 로드 실패.");
+                }
+            };
+
+            loadOperations.Add(loadAssetHandle);
+        }
+
+        // 모든 자산 로딩이 완료될 때까지 대기
+        var groupOp = Addressables.ResourceManager.CreateGenericGroupOperation(loadOperations);
+        yield return groupOp;
+
+        // 리소스 위치 핸들 해제
+        Addressables.Release(loadResourceLocationsHandle);
+
+        // 로드된 자산들 출력
+        foreach (var item in preloadedObjects)
+        {
+            Debug.Log($"{item.Key} - {item.Value.name}");
+        }
+    }
+
+    public void InstantiateAsset(string assetKey)
     {
-        Addressables.InstantiateAsync("test");
+        // 키로 자산을 비동기적으로 인스턴스화
+        Addressables.InstantiateAsync(assetKey);
     }
 }
